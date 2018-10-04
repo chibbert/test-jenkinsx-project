4549b6646f9a0bfc69eab94aad1585a58049fe23
Christopher Hibbert <christopher.hibbert@ericsson.com>
2018-10-04 14:48:16 +0100

    Test commit

--- src/main/java/com/example/testjenkinsxproject/DemoApplication.java
+++ src/main/java/com/example/testjenkinsxproject/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+// A comment
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
